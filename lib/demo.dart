bebac3808e5ce348d4cf31a5a6ecbc4ec6d066b8
LeTheLu <48428230+LeTheLu@users.noreply.github.com>
2021-11-15 23:13:33 +0700

    demo test

---
+++ lib/demo.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const GetUserName("abc"));
+}
+
+class GetUserName extends StatelessWidget {
+  final String documentId;
+
+  const GetUserName(this.documentId, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder<DocumentSnapshot>(
+          future: users.doc(documentId).get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+
+            if (snapshot.hasError) {
+              return Center(child: Text("Something went wrong"));
+            }
+
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return Center(child: Text("Document does not exist"));
+            }
+
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+              return Center(child: Text("Full Name: ${data['email']} ${data['name']}"));
+            }
+
+            return Center(child: Text("loading"));
+          },
+        ),
+      ),
+    );
+  }
+}
